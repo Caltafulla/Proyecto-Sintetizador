3e46bd9ec8a0b4076dc75832ee0495f9a707bc1a
jcarrenoa <jcarrenoa@uninorte.edu.co>
2022-05-31 11:55:40 -0500

    git push

--- DEMO/SonidoP.py
+++ DEMO/SonidoP.py
@@ -1,7 +1,7 @@
 import sounddevice as sd
 from VCO import Sine, Square, Saw
 import threading
-import matplotlib.pyplot as plt
+from grabadora import grabadora
 
 class SonidoP():
 
@@ -13,12 +13,12 @@ class SonidoP():
         SonidoP.GenerarSonido(wave)
 
     def playN(sonarO, onda, hz, x, volg):
-
         threads = []
-        
+        g = grabadora()
         for VCO in onda:
             VCO.tona()
             if VCO.onda != 0:
+                g.recording(VCO.f(VCO.v, hz(VCO.hzb, x), volg))
                 th = threading.Thread(target=lambda: sonarO(VCO.f(VCO.v, hz(VCO.hzb, x), volg)))
                 th.start()
                 threads.append(th)
